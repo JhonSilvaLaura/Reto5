3603df5a42d6fdce642629b2a8b421f5b293da2c
JhonSilvaLaura <jhon.silva.laura@vallegrande.edu.pe>
2024-11-11 17:00:51 -0500

    pagina de portafolio

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+app.use(express.static("public"));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'portafolio.html'));
+});
+
+const PORT = 3000;
+
+app.listen(PORT, () => console.log(`Servidor corriendo en http://localhost:${PORT}`));
